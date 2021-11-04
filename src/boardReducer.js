6e06ee83c42e0bdf8435e2beb6089c0298e6a017
martapalermo <marta.palermo421@gmail.com>
2021-11-03 18:00:30 -0700

    reducer!!

--- src/boardReducer.js
+++ src/boardReducer.js
@@ -1,12 +1,12 @@
-const defaultState = [['', '','',''], 0];
+const defaultState = [['','','',''], 0];
 
 
 export default function boardReducer(state = defaultState, action) {   
    
     switch (action.type) {
         case 'clickAction':
-            const value = state[action.x][action.y];
-            if (value === '' || value === 'white') {
+            const square = state[action.x][action.y];
+            if (square === '' || square === 'white') {
                 state[action.x][action.y] = 'black';
                 state[1] += 1;
             } else {
